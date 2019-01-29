4f52b185fe2c63e73f6804f29c08bf48c9f113a3
Michal <michal83h@gmail.com>
2019-01-29 10:26:12 +0100

    split fix

--- env.js
+++ env.js
@@ -2,7 +2,12 @@ const fs = require('fs');
 module.exports = function (splitted, done) {
   let txt = '';
   for (let i = 0; i < splitted.length; i++) {
-    txt += 'BUDDY_SPLIT_' + (i + 1) + '="' + splitted[i].join(' ') + '"\n';
+    const p = splitted[i].join(' ');
+    if (p.length > 100000) {
+      done(new Error('Too many file paths. Increase number of groups or decrease number of paths.'));
+      return;
+    }
+    txt += 'BUDDY_SPLIT_' + (i + 1) + '="' + p + '"\n';
   }
   fs.writeFile('split.sh', txt, done);
 };
